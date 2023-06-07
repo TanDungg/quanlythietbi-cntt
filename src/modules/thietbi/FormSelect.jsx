3d38a2f2894b18a0c07de961aba9524ee378589d
Pham Tan Dung <phamtandung@thaco.com.vn>
2023-06-07 13:32:27 +0700

    add

--- src/modules/thietbi/FormSelect.jsx
+++ src/modules/thietbi/FormSelect.jsx
@@ -2,44 +2,33 @@ import { Select } from 'antd';
 import './style.css';
 
 const FormSelect = () => (
-  <div>
-    <label>Hệ thống:</label>
-    <Select
-      className="form-select"
-      showSearch
-      placeholder="Search to Select"
-      optionFilterProp="children"
-      filterOption={(input, option) => (option?.label ?? '').includes(input)}
-      filterSort={(optionA, optionB) =>
-        (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-      }
-      options={[
-        {
-          value: '1',
-          label: 'Not Identified',
-        },
-        {
-          value: '2',
-          label: 'Closed',
-        },
-        {
-          value: '3',
-          label: 'Communicated',
-        },
-        {
-          value: '4',
-          label: 'Identified',
-        },
-        {
-          value: '5',
-          label: 'Resolved',
-        },
-        {
-          value: '6',
-          label: 'Cancelled',
-        },
-      ]}
-    />
+  <div className="form-select">
+    <div className="select-donvi">
+      <label>Hệ thống:</label>
+      <Select
+        showSearch
+        placeholder="Hệ thống"
+        optionFilterProp="children"
+        filterOption={(input, option) => (option?.label ?? '').includes(input)}
+        filterSort={(optionA, optionB) =>
+          (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+        }
+        options={[
+          {
+            value: '1',
+            label: 'Not Identified',
+          },
+          {
+            value: '2',
+            label: 'Closed',
+          },
+          {
+            value: '3',
+            label: 'Communicated',
+          },
+        ]}
+      />
+    </div>
   </div>
 );
 export default FormSelect;
